2f5c0668f7f2f56183ecc7c0b2fa7de97197a6cf
Aditijain23 <103481389+Aditijain23@users.noreply.github.com>
2023-04-25 00:10:10 +0530

    Update EX 1.sql

--- EX 1.sql
+++ EX 1.sql
@@ -4,8 +4,6 @@
 
 ### Q1) Create the tables DEPT and EMP as described below
 
-![Screenshot 2023-02-10 at 12 50 12 AM](https://user-images.githubusercontent.com/69889418/217915784-ed8a8ba1-94fb-40eb-adc9-947594e4c0dc.png)
-
 ``` SQL
 CREATE TABLE DEPT (
 	DEPTNO int,
@@ -13,7 +11,6 @@ CREATE TABLE DEPT (
 	LOC varchar(50)
 );
 ```
-![Screenshot 2023-02-10 at 12 58 32 AM](https://user-images.githubusercontent.com/69889418/217917495-bd764b24-f3c2-4cfa-8567-65caa3414609.png)
 
 ``` SQL
 CREATE TABLE EMP (
