ab5c737fd5ba7c390701722ac00a95de70fd218e
Aditijain23 <103481389+Aditijain23@users.noreply.github.com>
2023-04-25 00:09:03 +0530

    Update EX 2.sql

--- EX 2.sql
+++ EX 2.sql
@@ -1,7 +1,5 @@
 # Ex-2 Working with Data Manipulation commands
 
-![Screenshot 2023-02-10 at 3 10 42 AM](https://user-images.githubusercontent.com/69889418/217945479-1722f36f-1b18-4a3d-9d29-fd2a9c753832.png)
-
 ``` sql
 CREATE TABLE EMP (
     EMPNO int,
@@ -15,7 +13,6 @@ CREATE TABLE EMP (
 );
 ```
 
-![Screenshot 2023-02-10 at 3 11 18 AM](https://user-images.githubusercontent.com/69889418/217945590-4db7b3df-3342-4a49-9554-aecfdc81f27f.png)
 
 ``` SQL
 CREATE TABLE DEPT (
